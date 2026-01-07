9eaacbc62bb23b2f5e64635451bcc5a1e55386b0
kyeonghumkim0 <kimkhuna1@gmail.com>
2026-01-07 14:21:35 +0900

    Initial Commit

---
+++ Pagination_SwifUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pagination_SwifUI
+//
+//  Created by 김경훈 on 1/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pagination_SwifUI/Pagination_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pagination_SwifUIApp.swift
+//  Pagination_SwifUI
+//
+//  Created by 김경훈 on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Pagination_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
